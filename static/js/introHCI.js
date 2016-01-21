32ad3a6bb1e0f2cc315e652c85bb554b4a44b0f6
Chris Stringham <cstringham@s00-0000-3294-res.dynamic.ucsd.edu>
2016-01-21 11:59:19 -0800

    initial changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	onclick();
 })
 
 /*
@@ -15,4 +16,27 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#testjs").click(function(e) {
+		$("#project1").fadeOut();
+		$("#project2").fadeOut();
+		$("#project3").fadeOut();
+		$("#project4").fadeOut();
+		$("#project5").fadeOut();
+		$("#project6").fadeOut();
+		$("#project7").fadeOut();
+		$("#project8").fadeOut();
+	});
+	
+}
+function onclick() {
+	$("#reTestjs").click(function(e) {
+			$("#project1").fadeIn();
+			$("#project2").fadeIn();
+			$("#project3").fadeIn();
+			$("#project4").fadeIn();
+			$("#project5").fadeIn();
+			$("#project6").fadeIn();
+			$("#project7").fadeIn();
+			$("#project8").fadeIn();
+		});
 }
