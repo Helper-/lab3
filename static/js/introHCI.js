432a743833257616cb694a1a6030725ce5951117
Chris Stringham <cstringham@s00-0000-3294-res.dynamic.ucsd.edu>
2016-01-21 12:42:55 -0800

    Getting a little further

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -17,26 +17,18 @@ function initializePage() {
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 	$("#testjs").click(function(e) {
-		$("#project1").fadeOut();
-		$("#project2").fadeOut();
-		$("#project3").fadeOut();
-		$("#project4").fadeOut();
-		$("#project5").fadeOut();
-		$("#project6").fadeOut();
-		$("#project7").fadeOut();
-		$("#project8").fadeOut();
+		$('.project a').fadeOut();
 	});
+	
+	$("#project1").click(function(e) {
+		$("#project1").fadeToggle();
+	})
 	
 }
 function onclick() {
 	$("#reTestjs").click(function(e) {
-			$("#project1").fadeIn();
-			$("#project2").fadeIn();
-			$("#project3").fadeIn();
-			$("#project4").fadeIn();
-			$("#project5").fadeIn();
-			$("#project6").fadeIn();
-			$("#project7").fadeIn();
-			$("#project8").fadeIn();
+			$('.project a').fadeIn();
 		});
+		
+
 }
